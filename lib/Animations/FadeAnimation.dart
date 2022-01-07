92b71d76b4d13f49ff91a42c94b0dedc584470aa
Patel Parth Manishbhai <patelparthmanishbhai@Patels-MacBook-Air.local>
2022-01-07 14:11:06 +0530

    a

--- lib/Animations/FadeAnimation.dart
+++ lib/Animations/FadeAnimation.dart
@@ -7,7 +7,7 @@ class Add extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
+        title: Text("hell yaa...."),
       ),
     );
   }
--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
@@ -12,33 +10,30 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
   var Latitude = "";
   var Longtitude = "";
   var address = "";
 
-  Future<void> _updatePosition() async{
-    Position pos =await _determinePosition();
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  Future<void> _updatePosition() async {
+    Position pos = await _determinePosition();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
 
-
-    List pa = await placemarkFromCoordinates(position.latitude, position.longitude);
+    List pa =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
     setState(() {
       Latitude = position.latitude.toString();
       Longtitude = position.longitude.toString();
       address = pa[0].toString();
     });
   }
+
   Future<Position> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -46,27 +41,17 @@ class _SignInState extends State<SignIn> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -75,34 +60,30 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.amber[300],
         bottomOpacity: 20,
         elevation: 0,
-        title:const Text('hello'),
+        title: const Text('hello'),
         centerTitle: true,
       ),
-
-      body:Column(
+      body: Column(
         children: [
-              Text("Latitude :" + Latitude),
-              Text("Address : "),
-              Text(address),
-              Text("Longitude :" + Longtitude),
-
-
+          Text("Latitude :" + Latitude),
+          Text("Address : "),
+          Text(address),
+          Text("Longitude :" + Longtitude),
         ],
       ),
       floatingActionButton: Container(
         height: 80,
         width: 80,
-
         child: FloatingActionButton(
           hoverColor: Colors.green,
           backgroundColor: Colors.amber,
-          onPressed: (){
-                _updatePosition();
+          onPressed: () {
+            _updatePosition();
           },
-          child:const Icon(Icons.gps_fixed,
-          size: 30,
+          child: const Icon(
+            Icons.gps_fixed,
+            size: 30,
             color: Colors.black,
-
           ),
         ),
       ),
